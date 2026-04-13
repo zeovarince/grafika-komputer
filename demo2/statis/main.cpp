ab320191111551ea2641b50a8c6c068b8794c704
AinurRaftuzzaki <zakipheonix@gmail.com>
2026-04-13 14:54:41 +0700

    NRP randiom

--- demo2/statis/main.cpp
+++ demo2/statis/main.cpp
@@ -4,16 +4,23 @@
 #else
 #include <GL/glut.h>
 #endif
-#include<iostream>
+#include <iostream>
 #include <stdlib.h>
 #include <time.h>
 
 using namespace std;
-float postY=17.0;
-float postX=0.0;
+
+// Variabel Global
+float postY = 17.0;
+float postX = 0.0;
 int currentMaze = 1;
 
-void drawRect(float left, float top, float right, float bottom){
+// Tambahkan deklarasi posisi NRP di sini
+float nrpPosX = -5.0; // Contoh posisi awal X untuk NRP
+float nrpPosY = 0.0;  // Contoh posisi awal Y untuk NRP
+
+void drawRect(float left, float top, float right, float bottom)
+{
     glBegin(GL_POLYGON);
     glVertex2f(left, top);
     glVertex2f(right, top);
@@ -21,40 +28,64 @@ void drawRect(float left, float top, float right, float bottom){
     glVertex2f(left, bottom);
     glEnd();
 }
-void maze1(){
-    glColor3f(1.0, 0.0, 0.0); 
-    // Kotak Atas Kiri 
-    drawRect(-18.0, 18.0, -1.5, 16.0);
 
-    // Kotak Atas Kanan 
-    drawRect(1.5, 18.0, 18.0, 16.0);
+void drawNRP(float x, float y)
+{
+    glColor3f(0.0, 0.0, 1.0); // Warna Biru
+    float s = 0.2;            // Ukuran diperkecil dari 1.5 ke 0.45
 
-    // Kotak Bawah Kiri
-    drawRect(-18.0, -16.0, -1.5, -18.0);
+    // Angka 0
+    drawRect(x, y, x + s, y + 5 * s);
+    drawRect(x + 2 * s, y, x + 3 * s, y + 5 * s);
+    drawRect(x, y, x + 3 * s, y + s);
+    drawRect(x, y + 4 * s, x + 3 * s, y + 5 * s);
 
-    // Kotak Bawah Kanan
-    drawRect(1.5, -16.0, 18.0, -18.0);
+    // Angka 3
+    float ox = x + 4 * s;
+    drawRect(ox, y, ox + 3 * s, y + s);
+    drawRect(ox, y + 2 * s, ox + 3 * s, y + 3 * s);
+    drawRect(ox, y + 4 * s, ox + 3 * s, y + 5 * s);
+    drawRect(ox + 2 * s, y, ox + 3 * s, y + 5 * s);
 
-    // Kotak Kiri Penuh 
-    drawRect(-18.0, 16.0, -16.0, -16.0);
+    // Angka 4
+    ox = x + 8 * s;
+    drawRect(ox, y + 2 * s, ox + s, y + 5 * s);
+    drawRect(ox, y + 2 * s, ox + 3 * s, y + 3 * s);
+    drawRect(ox + 2 * s, y, ox + 3 * s, y + 5 * s);
+}
 
-    // Kotak Kanan Penuh
-    drawRect(16.0, 16.0, 18.0, -16.0);
-    
+void maze1()
+{
+    glColor3f(1.0, 0.0, 0.0);
+
+    // --- DINDING TERLUAR (BORDER) ---
+    // Atas
+    drawRect(-18.0, 18.0, -1.5, 17.0);
+    drawRect(1.5, 18.0, 18.0, 17.0);
+
+    // Bawah
+    drawRect(-18.0, -17.0, -1.5, -18.0);
+    drawRect(1.5, -17.0, 18.0, -18.0);
+
+    // Samping
+    drawRect(-18.0, 18.0, -17.0, -18.0); // Kiri
+    drawRect(17.0, 18.0, 18.0, -18.0);   // Kanan
+
+    // --- DINDING DALAM ---
     // Baris Y = 14
     drawRect(-14.5, 14.5, -5.5, 13.5);
     drawRect(-2.5, 14.5, 2.5, 13.5);
     drawRect(5.5, 14.5, 10.5, 13.5);
-    drawRect(13.5, 14.5, 18.0, 13.5);
+    drawRect(13.5, 14.5, 17.0, 13.5);
 
     // Baris Y = 10
-    drawRect(-18.0, 10.5, -13.5, 9.5);
+    drawRect(-17.0, 10.5, -13.5, 9.5);
     drawRect(-10.5, 10.5, -5.5, 9.5);
     drawRect(1.5, 10.5, 6.5, 9.5);
-    drawRect(9.5, 10.5, 18.0, 9.5);
+    drawRect(9.5, 10.5, 17.0, 9.5);
 
     // Baris Y = 6
-    drawRect(-18.0, 6.5, -9.5, 5.5);
+    drawRect(-17.0, 6.5, -9.5, 5.5);
     drawRect(-6.5, 6.5, 2.5, 5.5);
     drawRect(5.5, 6.5, 14.5, 5.5);
 
@@ -63,7 +94,7 @@ void maze1(){
     drawRect(-2.5, 2.5, 2.5, 1.5);
 
     // Baris Y = -2
-    drawRect(-18.0, -1.5, -5.5, -2.5);
+    drawRect(-17.0, -1.5, -5.5, -2.5);
     drawRect(1.5, -1.5, 10.5, -2.5);
     drawRect(9.5, -1.5, 14.5, -2.5);
 
@@ -71,126 +102,150 @@ void maze1(){
     drawRect(-14.5, -5.5, -9.5, -6.5);
     drawRect(-10.5, -5.5, -1.5, -6.5);
     drawRect(1.5, -5.5, 10.5, -6.5);
-    drawRect(13.5, -5.5, 18.0, -6.5);
+    drawRect(13.5, -5.5, 17.0, -6.5);
 
-    // Baris Y = -10
-    drawRect(-18.0, -9.5, -9.5, -10.5);
-    drawRect(-6.5, -9.5, -1.5, -10.5);
+    // --- GARIS BARU (SESUAI GAMBAR USER) ---
+    // Menghubungkan celah di area bawah
+    drawRect(-6.5, -9.5, 1.5, -10.5);
+
+    // Baris Y = -10 (Sisa)
+    drawRect(-17.0, -9.5, -9.5, -10.5);
     drawRect(1.5, -9.5, 6.5, -10.5);
     drawRect(9.5, -9.5, 14.5, -10.5);
 
     // Baris Y = -14
-    drawRect(-18.0, -13.5, -13.5, -14.5);
+    drawRect(-17.0, -13.5, -13.5, -14.5);
     drawRect(-10.5, -13.5, -1.5, -14.5);
     drawRect(5.5, -13.5, 10.5, -14.5);
 
-    // Kolom X = -14
+    // Kolom Vertikal
     drawRect(-14.5, 14.5, -13.5, 9.5);
     drawRect(-14.5, 2.5, -13.5, -6.5);
     drawRect(-14.5, -9.5, -13.5, -14.5);
-
-    // Kolom X = -10
-    drawRect(-10.5, 18.0, -9.5, 13.5);
+    drawRect(-10.5, 17.0, -9.5, 13.5);
     drawRect(-10.5, 10.5, -9.5, 5.5);
     drawRect(-10.5, -1.5, -9.5, -6.5);
     drawRect(-10.5, -9.5, -9.5, -14.5);
-
-    // Kolom X = -6
     drawRect(-6.5, 10.5, -5.5, 5.5);
     drawRect(-6.5, 6.5, -5.5, 1.5);
     drawRect(-6.5, -5.5, -5.5, -10.5);
-
-    // Kolom X = -2
     drawRect(-2.5, 14.5, -1.5, 9.5);
     drawRect(-2.5, 6.5, -1.5, -2.5);
-
-    // Kolom X = 2
-    drawRect(1.5, 18.0, 2.5, 13.5);
-    drawRect(1.5, 14.5, 2.5, 9.5);
-    drawRect(1.5, -5.5, 2.5, -18.0);
-
-    // Kolom X = 6
     drawRect(5.5, 10.5, 6.5, 5.5);
     drawRect(5.5, 6.5, 6.5, -10.5);
-
-    // Kolom X = 10
     drawRect(9.5, 14.5, 10.5, 9.5);
     drawRect(9.5, 6.5, 10.5, 1.5);
     drawRect(9.5, -1.5, 10.5, -6.5);
     drawRect(9.5, -9.5, 10.5, -14.5);
-
-    // Kolom X = 14
     drawRect(13.5, 6.5, 14.5, 1.5);
     drawRect(13.5, 2.5, 14.5, -2.5);
     drawRect(13.5, -5.5, 14.5, -10.5);
-    drawRect(13.5, -9.5, 14.5, -18.0);
+    drawRect(13.5, -9.5, 14.5, -17.0);
+}
+void maze2()
+{
+    // Tempat untuk menggambar maze 2
 }
 
-void maze2(){
+void randomizeNRP()
+{
+    // Daftar koordinat (x, y) yang merupakan jalan kosong di maze kamu
+    // Kamu bisa menambah atau menyesuaikan angka-angka ini
+    float safePointsX[] = {-12.0, -8.0, 0.0, 8.0, 12.0, -15.0, 15.0, 4.0, -4.0};
+    float safePointsY[] = {12.0, 8.0, 4.0, 0.0, -4.0, -8.0, -12.0, 15.0, -15.0};
 
+    int jumlahTitik = sizeof(safePointsX) / sizeof(safePointsX[0]);
+    int index = rand() % jumlahTitik;
+
+    nrpPosX = safePointsX[index];
+    nrpPosY = safePointsY[index];
 }
 
-void display(){
+void display()
+{
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // Menggambar Player (Kotak Hitam)
     glBegin(GL_POLYGON);
-    glColor3f(0.0,0.0,0.0);
-    glVertex2f(-1.0 + postX ,1.0 + postY);
-    glVertex2f(1.0 + postX ,1.0 + postY);
-    glVertex2f(1.0 + postX ,-1.0 + postY);
-    glVertex2f(-1.0 + postX ,-1.0 + postY);
+    glColor3f(0.0, 0.0, 0.0);
+    glVertex2f(-0.9 + postX, 0.9 + postY);
+    glVertex2f(0.9 + postX, 0.9 + postY);
+    glVertex2f(0.9 + postX, -0.9 + postY);
+    glVertex2f(-0.9 + postX, -0.9 + postY);
     glEnd();
 
-    if(currentMaze == 1){
+    // Gambar NRP
+    drawNRP(nrpPosX, nrpPosY);
+
+    // Gambar Maze
+    if (currentMaze == 1)
+    {
         maze1();
-    } else {
+    }
+    else
+    {
         maze2();
     }
+
     glFlush();
 }
 
-void myinit(){
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(-18.0,18.0,-18.0,18.0);
-	glMatrixMode(GL_MODELVIEW);
-	glClearColor(1.0,1.0,1.0,1.0);
-	glColor3f(1.0,1.0,1.0);
+void myinit()
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-18.0, 18.0, -18.0, 18.0);
+    glMatrixMode(GL_MODELVIEW);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glColor3f(1.0, 1.0, 1.0);
 }
 
-void input(unsigned char key, int x, int y){
-    if(key == 'q') {
-       exit(0);
+void input(unsigned char key, int x, int y)
+{
+    if (key == 'q')
+    {
+        exit(0);
     }
-    if(key == 'w' || key == 'W'){
-        postY +=0.1;
+    if (key == 'w' || key == 'W')
+    {
+        postY += 0.5;
+    }
+    if (key == 's' || key == 'S')
+    {
+        postY -= 0.5;
+    }
+    if (key == 'a' || key == 'A')
+    {
+        postX -= 0.5;
+    }
+    if (key == 'd' || key == 'D')
+    {
+        postX += 0.5;
+    }
 
-    }
-    if(key=='s' || key == 'S'){
-        postY -=0.1;
-    }
-    if (key=='a' || key=='A'){
-        postX -=0.1;
-    }
-    if (key=='d' || key=='D'){
-        postX +=0.1;
-    }
-    if(key == 'c' || key == 'C'){
+    // MODIFIKASI DI SINI
+    if (key == 'c' || key == 'C')
+    {
         currentMaze = (currentMaze == 1) ? 2 : 1;
-        glutPostRedisplay(); 
+        randomizeNRP(); // Panggil fungsi titik aman
     }
-    display();
+
+    glutPostRedisplay();
 }
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    randomizeNRP(); // Set posisi awal secara amana
 
-int main(int argc, char* argv[]){
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(750,750);
-	glutCreateWindow("demo 2 maze statis");
-	
-	glutDisplayFunc(display);
-	glutKeyboardFunc(input);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(750, 750);
+    glutCreateWindow("demo 2 maze statis");
 
-	myinit();
-	glutMainLoop();
-	return 0;
+    glutDisplayFunc(display);
+    glutKeyboardFunc(input);
+
+    myinit();
+    glutMainLoop();
+    return 0;
 }
